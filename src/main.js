6589be117bcd527b04397bca88be25837c44acf4
wikihong <hjpChinash@163.com>
2018-08-28 17:23:00 +0800

    0828-底部导航

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Stepper, PullRefresh, Tab, Tabs, Button, Row, Col, Swipe, SwipeItem, Lazyload, List, Field, NavBar} from 'vant'
-Vue.use(Stepper).use(PullRefresh).use(Tab).use(Tabs).use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(Field).use(NavBar)
+import {Tabbar, TabbarItem, Stepper, PullRefresh, Tab, Tabs, Button, Row, Col, Swipe, SwipeItem, Lazyload, List, Field, NavBar} from 'vant'
+Vue.use(Tabbar).use(TabbarItem).use(Stepper).use(PullRefresh).use(Tab).use(Tabs).use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(Field).use(NavBar)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,21 @@ import Login from '@/components/pages/Login'
 import Goods from '@/components/pages/Goods'
 import CategoryList from '@/components/pages/CategoryList'
 import Cart from '@/components/pages/Cart'
+import Main from '@/components/pages/Main'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', name: 'ShoppingMall', component: ShoppingMall },
+    { path: '/main', name: 'Main', component: Main,
+      children: [
+        { path: '/', name: 'ShoppingMall', component: ShoppingMall },
+        { path: '/categoryList', name: 'CategoryList', component: CategoryList },
+        { path: '/cart', name: 'Cart', component: Cart }
+      ]
+    },
     { path: '/register', name: 'Register', component: Register },
     { path: '/login', name: 'Login', component: Login },
     { path: '/goods', name: 'Goods', component: Goods },
-    { path: '/categoryList', name: 'CategoryList', component: CategoryList },
-    { path: '/cart', name: 'Cart', component: Cart }
   ]
 })
